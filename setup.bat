bb5b047e4cc09314f475943b28a2af1c44f0db4d
DeflatedPickle <DeflatedPickle@gmail.com>
2017-08-11 03:11:20 +0100

    Included nbt.world as a hidden import

--- setup.bat
+++ setup.bat
@@ -1,10 +1,12 @@
 pyinstaller ^
 	--console ^
 	--onedir ^
+	--noconfirm ^
 	--name="Quiver" ^
 	--icon=quiver.ico ^
 	--add-data="properties.json;." ^
 	--add-data="quiver.ico;." ^
 	--add-data="icons/;icons/" ^
 	--clean ^
+	--hidden-import="nbt.world" ^
 	window.py
