cd4e87429431b25331e88b3c0b6f097963cf8c6a
DeflatedPickle <DeflatedPickle@gmail.com>
2020-11-22 07:05:39 +0000

    Added search toolbars

--- foldertree/src/main/kotlin/com/deflatedpickle/quiver/foldertree/Component.kt
+++ foldertree/src/main/kotlin/com/deflatedpickle/quiver/foldertree/Component.kt
@@ -4,13 +4,14 @@ package com.deflatedpickle.quiver.foldertree
 
 import com.deflatedpickle.haruhi.component.PluginPanel
 import com.deflatedpickle.rawky.ui.constraints.FillBothFinishLine
+import java.awt.BorderLayout
 import java.awt.GridBagLayout
 import javax.swing.JScrollPane
 
 object Component : PluginPanel() {
     init {
-        this.layout = GridBagLayout()
+        this.layout = BorderLayout()
 
-        this.add(JScrollPane(FolderTree), FillBothFinishLine)
+        this.add(JScrollPane(FolderTree), BorderLayout.CENTER)
     }
 }
