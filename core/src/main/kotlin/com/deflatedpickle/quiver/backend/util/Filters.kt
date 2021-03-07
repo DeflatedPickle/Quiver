bd27a28129bdbc056aed11a36a96b63c57746945
DeflatedPickle <deflatedpickle@gmail.com>
2021-03-07 16:24:19 +0000

    Added some validation checks before creating a new pack

--- core/src/main/kotlin/com/deflatedpickle/quiver/backend/util/Filters.kt
+++ core/src/main/kotlin/com/deflatedpickle/quiver/backend/util/Filters.kt
@@ -1,4 +1,4 @@
-/* Copyright (c) 2020 DeflatedPickle under the MIT license */
+/* Copyright (c) 2020-2021 DeflatedPickle under the MIT license */
 
 package com.deflatedpickle.quiver.backend.util
 
@@ -8,5 +8,5 @@ object Filters {
     val FILE = PatternFilter("""[^\\./:*?\"<>|]*""")
     val PATH_WINDOWS = PatternFilter("""[A-Za-z]:[^:*?"<>|]*""")
     // https://stackoverflow.com/a/28989719
-    val PATH_UNIX = PatternFilter("""^(/[^/ \n]*)+/?$""")
+    val PATH_UNIX = PatternFilter("""^(/[^/\n]*)+/?$""")
 }
